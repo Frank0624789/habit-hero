80c5f443a446d31439f7067e2c158fc5d202ece8
Frank0624789 <Frank0624789@gmail.com>
2026-06-15 22:31:24 -0400

    Correctly implemented all of it into repo and began sessionstate stuff

--- app.py
+++ app.py
@@ -5,3 +5,6 @@ st.set_page_config(page_title="Habit Hero", page_icon="🐱‍👓", layout="cen
 st.title("🐱‍👓 Habit Hero: Level Up Life 🎮")
 st.subheader("Turn your daily routine into a series of engaging quests!")
 
+#state memory vault init
+if "xp" not in st.session_state:
+    st.session_state.xp =0
