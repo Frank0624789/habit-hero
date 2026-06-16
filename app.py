b7175d5e41e3504ac4dd985e55901c8ba817e98d
Frank0624789 <Frank0624789@gmail.com>
2026-06-15 22:17:19 -0400

    Beginning of App.py and moved it into repo

---
+++ app.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+#App title/app-looks config
+st.set_page_config(page_title="Habit Hero", page_icon="🐱‍👓", layout="centered")
+st.title("🐱‍👓 Habit Hero: Level Up Life 🎮")
+st.subheader("Turn your daily routine into a series of engaging quests!")
+
